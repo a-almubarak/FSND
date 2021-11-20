ca7831df3e09b278013949589e88ba6763eafaea
iabdull4h <euh.27@hotmail.com>
2021-11-20 04:19:09 +0300

    first commit

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,15 +2,49 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db, db_create_all, Movie, Actor
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
 
-  return app
 
-APP = create_app()
+# create and configure the app
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+db_create_all()
+
+@app.route('/movies',methods=['GET'])
+def get_movies():
+    pass
+
+@app.route('/movies',methods=['POST'])
+def post_movies():
+    pass
+
+@app.route('/movies/<int:movie_id>',methods=['PATCH'])
+def patch_movies(movie_id):
+    pass
+
+@app.route('/movies/<int:movie_id>',methods=['DELETE'])
+def delete_movies(movie_id):
+    pass
+
+@app.route('/actors',methods=['GET'])
+def get_actors():
+    pass
+
+@app.route('/actors',methods=['POST'])
+def post_actors():
+    pass
+
+@app.route('/actors/<int:actor_id>',methods=['PATCH'])
+def patch_actors(actor_id):
+    pass
+
+@app.route('/actors/<int:actor_id>',methods=['DELETE'])
+def delete_actors(actor_id):
+    pass
+
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,51 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
+
+database_path = ''
+
+db = SQLAlchemy()
+
+def setup_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    
+def db_create_all():
+    db.create_all()
+
+association_table = db.Table('association',db.Model.metadata,
+                            db.Column('movie_id',db.Integer,db.ForeignKey('movie.id'),primary_key=True),
+                            db.Column('actor_id',db.Integer,db.ForeignKey('actor.id'),primary_key=True))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.String)
+    actors = db.relationship("Actor",secondary=association_table,lazy='subquery',backref=db.backref('movies',lazy=True))
+
+    def format(self):
+        return jsonify({
+            'id':self.id,
+            'title':self.title,
+            'release_date':self.release_date,
+            'actors':[i.id for i in self.actors]
+        })
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String)
+
+    def format(self):
+        return jsonify({
+            'id':self.id,
+            'name':self.name,
+            'age':self.age,
+            'gender':self.gender,
+            'movies':[i.id for i in self.movies]
+        })
 
